23a7c9397d827bc9ecb0fd79f84cdef48e501808
qyme <kai.ning.vs@gmail.com>
2025-02-24 06:55:08 +0100

    L6: Count Characters

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from stats import get_num_words
+from stats import get_char_count
 
 def main ():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
+    num_char = get_char_count(text)
 
     print(f"{num_words} words found in the document")
+    for char in num_char:
+        print(f"{char}: {num_char[char]}")
 
 def get_book_text (path_to_file):
     with open(path_to_file) as f: 
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    lowercase = text.lower()
+    symbols_count = {}
+    for symbols in lowercase:
+        if symbols in symbols_count:
+            symbols_count[symbols] += 1
+        else:
+            symbols_count[symbols] = 1
+    return symbols_count
