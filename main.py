7b4d9d1a73ce8d2d3cc0ab18dd8900d1e5464492
qyme <kai.ning.vs@gmail.com>
2025-02-24 22:25:18 +0100

    CH3 L3: submit the link to your repo !

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys 
 from stats import get_num_words
 from stats import get_char_count
 from stats import sorted_char_count
 
 def main ():
-    path_to_file = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_file = sys.argv[1]
+
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
     num_char = get_char_count(text)
@@ -23,7 +29,6 @@ def main ():
 def get_book_text (path_to_file):
     with open(path_to_file) as f: 
         return f.read()
-
 
 
 main()
